f08fccecaba71eef81cce384b314ecc0e7878d9c
srfrnk <srfrnk@gmail.com>
2018-07-29 15:08:10 +0300

    feat: added class for spell checker

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,13 @@ const PLUGIN_NAME = 'gulp-ts-spellcheck';
 const through = require('through2');
 const gutil = require('gulp-util');
 
+import SpellChecker from './spell-checker';
+
 const PluginError = gutil.PluginError;
 
-function processFile(input: string): string {
-    // throw new PluginError(PLUGIN_NAME, 'wtf????');
-    return '';
-}
-
 export default function gulpPlugin(options: any) {
+    const processor = new SpellChecker(options);
+
     return through.obj(function(file: any, enc: any, callback: any) {
         try {
             // Empty file and directory not supported
@@ -24,7 +23,7 @@ export default function gulpPlugin(options: any) {
             if (isBuffer) {
                 const aFile = new gutil.File();
                 aFile.path = file.path;
-                aFile.contents = new Buffer(processFile(file.contents));;
+                aFile.contents = new Buffer(processor.process(file.contents));
                 callback(null, aFile);
             } else {
                 this.emit('error', new PluginError(PLUGIN_NAME, 'Only Buffer format is supported'));
---
+++ src/spell-checker.ts
@@ -0,0 +1,11 @@
+export default class SpellChecker {
+    private dict: string[];
+
+    constructor({ dict: string }: any) {
+        this.dict = this.dict;
+    }
+
+    public process(input: string): string {
+        return '';
+    }
+}
