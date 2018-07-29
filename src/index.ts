cfd57998da62a8d8ef63b5cede2bc1497e1a9b2f
srfrnk <srfrnk@gmail.com>
2018-07-29 13:58:38 +0300

    fix: ready for tests

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
 const PLUGIN_NAME = 'gulp-ts-spellcheck';
 
-var through = require('through2'),
-    gutil = require('gulp-util'),
-    PluginError = gutil.PluginError;
+// tslint:disable:no-var-requires
+const through = require('through2');
+const gutil = require('gulp-util');
+
+const PluginError = gutil.PluginError;
 
 function processFile(input: string): string {
-    throw new PluginError(PLUGIN_NAME, 'wtf????');
+    // throw new PluginError(PLUGIN_NAME, 'wtf????');
     return '';
 }
 
 export default function gulpPlugin(options: any) {
-    return through.obj(function (file: any, enc: any, callback: any) {
+    return through.obj(function(file: any, enc: any, callback: any) {
         try {
-            //Empty file and directory not supported
+            // Empty file and directory not supported
             if (file === null || file.isDirectory()) {
                 this.push(file);
                 return callback();
@@ -25,13 +27,12 @@ export default function gulpPlugin(options: any) {
                 aFile.contents = new Buffer(processFile(file.contents));;
                 callback(null, aFile);
             } else {
-                this.emit('error',new PluginError(PLUGIN_NAME, 'Only Buffer format is supported'));
+                this.emit('error', new PluginError(PLUGIN_NAME, 'Only Buffer format is supported'));
                 callback();
             }
-        }
-        catch (error) {
-            this.emit('error',error);
+        } catch (error) {
+            this.emit('error', error);
             callback();
         }
     });
-};
+}
--- test/gulp.test.js
+++ test/gulp.test.js
@@ -4,7 +4,9 @@ const gulpTsSpellcheck = require('../build/index.js').default;
 
 async function gulpProcess(plugin, inputString) {
     var fakeFile = new File({
-        cwd: '/', base: '/', path: '/file',
+        cwd: '/',
+        base: '/',
+        path: '/file',
         contents: new Buffer(inputString)
     });
 
@@ -16,8 +18,7 @@ async function gulpProcess(plugin, inputString) {
         plugin.once('data', function (file) {
             if (!file.isBuffer()) {
                 reject('Not a buffer');
-            }
-            else {
+            } else {
                 resolve(file.contents.toString('utf8'));
             }
         });
@@ -26,39 +27,16 @@ async function gulpProcess(plugin, inputString) {
     return await promise;
 }
 
-async function expectPromiseToThrow(promise,expectedError) {
-    try {
-        const result = await promise;
-        throw new Error(`'Expected function to throw ${expectedError}, nothing was thrown`);
-    }
-    catch (error) {
-        expect(error).toBe(expectedError);
-    }
-}
-
 describe('gulp package', () => {
     it('should run', async () => {
-        // expect(await gulpProcess(gulpTsSpellcheck(), 'buffer with those contents')).toEqual('');
-        expectPromiseToThrow(gulpProcess(gulpTsSpellcheck(), 'buffer with those contents'));
-        /*         var fakeFile = new File({
-                    cwd: '/', base: '/', path: '/file',
-                    contents: new Buffer('buffer with those contents')
-                });
-
-                var gulpTsSpellcheck1 = gulpTsSpellcheck();
-
-                gulpTsSpellcheck1.on('error', (err) => {
-                    done.fail(`Error: ${err}`);
-                    done();
-                });
-
-                gulpTsSpellcheck1.write(fakeFile);
-
-                gulpTsSpellcheck1.once('data', function (file) {
-                    expect(file.isBuffer()).toBeTruthy();
-                    expect(file.contents.toString('utf8')).toEqual('');
-                    done();
-                });
-         */
-    })
+        const result = await gulpProcess(gulpTsSpellcheck(), 'buffer with those contents');
+    });
+    it('should throw', async () => {
+        try {
+            const result = await gulpProcess(gulpTsSpellcheck(), 'buffer with those contents');
+        } catch (error) {
+            return;
+        }
+        throw new Error('No exception thrown');
+    });
 });
