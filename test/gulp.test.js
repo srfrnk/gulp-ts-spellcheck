543891f56db039256686dc7caefb4ac5214d337e
srfrnk <srfrnk@gmail.com>
2018-08-04 20:26:39 +0300

    fix: gulp tests

--- test/gulp.test.js
+++ test/gulp.test.js
@@ -1,47 +1,47 @@
 var from = require('from2');
 var File = require('vinyl');
-const gulpTsSpellcheck = require('../build/index.js').default;
 const fs = require('fs');
 
-async function gulpProcess(plugin, inputString) {
-    var fakeFile = new File({
-        cwd: '/',
-        base: '/',
-        path: '/file',
-        contents: new Buffer(inputString)
-    });
-
-    const promise = new Promise((resolve, reject) => {
-        plugin.on('error', (error) => {
-            reject(`Exception thrown: ${error.message}`);
-        });
-
-        plugin.once('data', function (file) {
-            if (!file.isBuffer()) {
-                reject('Not a buffer');
-            } else {
-                resolve(file.contents.toString('utf8'));
-            }
-        });
-    });
-    plugin.write(fakeFile);
-    return await promise;
-}
-
 describe('gulp package', () => {
-    it('should run', async () => {
-        const plugin = gulpTsSpellcheck({ dictionary: ['Func'] });
-        const result = await gulpProcess(plugin, fs.readFileSync('./test/test-files/correct.checked.ts').toString('utf8'));
-        expect(result).toBe('');
+    beforeEach(() => {
+        jest.resetModules();
+        jest.resetAllMocks();
     });
-    /*
-         it('should throw', async () => {
-            try {
-                const result = await gulpProcess(gulpTsSpellcheck(), 'buffer with those contents');
-            } catch (error) {
-                return;
-            }
-            throw new Error('No exception thrown');
-        });
-     */
+
+    it('should pass when no spelling errors exist', async () => {
+        jest.doMock('spellchecker', () => ({
+            add: () => { },
+            isMisspelled: () => false
+        }));
+
+        const gulp = require('gulp');
+        const spellCheck = require('../build/index').default;
+        gulp.src('test/test-files/*.ts')
+            .on('error', function (err) {
+                done(err);
+            })
+            .pipe(spellCheck({ dictionary: ['Func'] }))
+            .pipe(spellCheck.report({}));
+    });
+
+    it('should error when spelling errors exist', (done) => {
+        jest.doMock('spellchecker', () => ({
+            add: () => { },
+            isMisspelled: () => true
+        }));
+
+        const gulp = require('gulp');
+        const spellCheck = require('../build/index').default;
+        let error = null;
+        gulp.src('test/test-files/*.ts')
+            .on('end', function () {
+                expect(error).toBeDefined();
+                done();
+            })
+            .pipe(spellCheck({ dictionary: ['Func'] }))
+            .pipe(spellCheck.report({}))
+            .on('error', function (err) {
+                error = err;
+            });
+    });
 });
